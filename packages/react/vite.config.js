a48bf8124a34591f5cf509e6c87d7174b7ea176b
Michael Feher <michael.feher@algorand.foundation>
2023-09-06 21:04:04 -0500

    chore: remove library build

--- packages/react/vite.config.js
+++ packages/react/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{
-    lib:{
-        entry: 'src/index.tsx',
-    }
-  }
+  // build:{
+  //   lib:{
+  //       entry: 'src/index.tsx',
+  //   }
+  // }
 })
