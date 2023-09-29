de2b4a86e957a827e8d86e764deb74d82beb63ea
Michael Feher <michael.feher@algorand.foundation>
2023-09-29 18:44:03 -0500

    perf: use shallow routing

--- packages/react/src/routes/QueryTableRow.jsx
+++ packages/react/src/routes/QueryTableRow.jsx
@@ -1,9 +1,11 @@
+import {Link} from "react-router-dom";
+
 export default function QueryTableRow({name, type, query}){
     const loadingColor = query.isError ? "red" : query.isLoading? "yellow" : "green"
     const errorColor = query.isError && ! query.isLoading ? "red" : query.isLoading ? "yellow" : "green"
     return (
         <tr>
-            <td><a href={`/${type}/${name}`}>{name}</a></td>
+            <td><Link to={`/${type}/${name}`}>{name}</Link></td>
             <td style={{color: loadingColor}}>{query.isLoading ? "true": "false"}</td>
             <td style={{color: errorColor}}>{query.isError ? "true": "false"}</td>
         </tr>
