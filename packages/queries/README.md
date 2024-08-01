a646fc22686dac5fa1a2d5d2aa695a194a78ac10
Michael Feher <michael.feher@algorand.foundation>
2024-07-31 22:15:26 -0400

    🔥 remove missed files

--- packages/queries/README.md
+++
@@ -1,3 +0,0 @@
-# @awesome-algorand/use-algorand-query
-
-Collection of query objects to use in different frontends.
--- packages/queries/test.js
+++
@@ -1,7 +0,0 @@
-import {AlgodClient} from "@awesome-algorand/algod-fetch"
-
-const client = new AlgodClient( {
-    BASE: 'https://testnet-api.algonode.cloud'
-})
-
-console.log(client)
